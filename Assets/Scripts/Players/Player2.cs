530d92c68c5cb7cfdb874f1c0a1deb371b4294b2
damjali <adamrazali54@gmail.com>
2026-04-18 16:35:37 +0800

    sebelum fucked up gg enemy movement aniimation

--- Assets/Scripts/Players/Player2.cs
+++ Assets/Scripts/Players/Player2.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
+
 namespace Players
 {
     public class Player2 : Player
     {
-        public void Start()
+        // Start is used for initialization
+        void Start()
         {
+            // Ensure abilities are active
             haveDown = false;
             haveLeft = false;
             haveRight = false;
             haveUp = false;
         }
-        
+
         public override void Update()
         {
+            // 1. Reset movement vector in base class
             base.Update();
+
+            // 2. Capture Keyboard Input (WASD)
             if (Input.GetKey(KeyCode.LeftArrow) && haveLeft)
                 movement.x = -1;
-            if (Input.GetKey(KeyCode.RightArrow) && haveRight)
+            else if (Input.GetKey(KeyCode.RightArrow) && haveRight)
                 movement.x = 1;
+
             if (Input.GetKey(KeyCode.UpArrow) && haveUp)
                 movement.y = 1;
-            if (Input.GetKey(KeyCode.DownArrow) && haveDown)
+            else if (Input.GetKey(KeyCode.DownArrow) && haveDown)
                 movement.y = -1;
+
+            // 3. Update the animator parameters
+            UpdateAnimations();
         }
     }
 }
